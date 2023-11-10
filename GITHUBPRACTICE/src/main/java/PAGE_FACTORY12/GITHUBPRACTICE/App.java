ffdcb44b094e9dc7d86dd1de2063a9bfc623b7cc
kasi <kasi@LAPTOP-A7DLPHTI>
2023-11-11 00:29:22 +0530

    commit on Nov10

---
+++ GITHUBPRACTICE/src/main/java/PAGE_FACTORY12/GITHUBPRACTICE/App.java
@@ -0,0 +1,13 @@
+package PAGE_FACTORY12.GITHUBPRACTICE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
